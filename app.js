801c95414ce94f14813b84b384e6f1a424b13278
omar <sfaxi33omar@gmail.com>
2021-08-31 02:31:34 +0100

    ui

--- app.js
+++ app.js
@@ -6,13 +6,36 @@ app.set('views','./views')
 app.set('view engine', 'ejs');
 
 
-
-
+app.get('/index',(req,res)=>{
+    res.render('index')
+    
+    });
 
 app.get('/',(req,res)=>{
-res.render('index')
+res.render('basic')
 
 });
+
+app.get('/views/basic.ejs',(req,res)=>{
+    res.render('basic')
+    
+    });
+
+
+app.get('/views/template.ejs',(req,res)=>{
+    res.render('template')
+    
+    });
+
+    app.get('/views/template.ejs',(req,res)=>{
+        res.render('template')
+        
+        });
+
+        app.get('/views/social.ejs',(req,res)=>{
+            res.render('social')
+            
+            });
 
 
 app.listen(8000,()=>console.log('server started...'));
